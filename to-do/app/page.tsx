cc59a20b0e019cbb7b42044a6ba57cb2bfa978ec
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2024-10-07 18:42:55 +0600

    up

---
+++ to-do/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center underline">Next js</h1>
+      <button className="btn">Click me</button>
+    </main>
+  );
+}
